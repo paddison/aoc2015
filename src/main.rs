6769cb21a8fe0023c3d9c59ad8f123e8be14f621
Baumann Patrick <pb083@hdm-stuttgart.de>
2022-10-26 12:49:48 +0200

    rewrite d7

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ mod d3;
 mod d4;
 mod d5;
 mod d6;
+mod d7;
 
 fn main() {
+    let start_all = Instant::now();
     let start = Instant::now();
     let result = d1::get_solution_1();
     let end = start.elapsed().as_micros();
@@ -67,4 +69,17 @@ fn main() {
     let result = d6::get_solution_2(); 
     let end = start.elapsed().as_micros();
     println!("Day 6.2\t{}us\t{}", end, result);
+
+    let start = Instant::now();
+    let result = d7::get_solution_1(); 
+    let end = start.elapsed().as_micros();
+    println!("Day 7.1\t{}us\t{}", end, result);
+
+    let start = Instant::now();
+    let result = d7::get_solution_2(); 
+    let end = start.elapsed().as_micros();
+    println!("Day 7.2\t{}us\t{}", end, result);
+
+    let end_all = start_all.elapsed().as_micros();
+    println!("Total: {}us", end_all)
 }
